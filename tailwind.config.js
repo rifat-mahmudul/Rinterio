9f95cd9be63dbcce58f77eaea2e5277e6827e96d
Mahmudul Hasan Rifat <mahmudulhasanrifat201@gmail.com>
2024-08-22 08:31:30 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"],
+    theme: {
+        fontFamily: {
+            manrope:["Manrope"]
+        },
+
+        colors: {
+            'color-primary': '#abef5f',
+            'color-background': '#f4f4f4'
+        },
+
+        extend: {},
+    },
+    plugins: [],
+}
